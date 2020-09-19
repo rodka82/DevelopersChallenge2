a671df1b3f8ea4e94256959f920058aeaf566061
Rodrigo <rduclerc@gmail.com>
2020-09-19 15:28:25 -0300

    Initial domain entitite  BankTransaction and enum BankTransactionType

---
+++ src/DeveloperChallenge/DeveloperChallenge.Domain/Enums/BankTransactionType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeveloperChallenge.Domain.Enums
+{
+    public enum BankTransactionType
+    {
+        Debit,
+        Credit
+    }
+}
