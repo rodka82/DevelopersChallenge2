9c603d71afba8be8b4ad84e3343b68fa49f3c096
Rodrigo <rduclerc@gmail.com>
2020-09-19 15:29:35 -0300

    OFXParser exceptions

---
+++ src/DeveloperChallenge/DeveloperChallenge.Application/Exceptions/OfxException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace DeveloperChallenge.Application.Exceptions
+{
+    [Serializable]
+    public class OfxException : Exception
+    {
+        public OfxException()
+        {
+        }
+
+        public OfxException(string message) : base(message)
+        {
+        }
+
+        public OfxException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected OfxException(
+            SerializationInfo info,
+            StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
---
+++ src/DeveloperChallenge/DeveloperChallenge.Application/Exceptions/OfxParseException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace DeveloperChallenge.Application.Exceptions
+{
+    [Serializable]
+    public class OfxParseException : OfxException
+    {
+        public OfxParseException()
+        {
+        }
+
+        public OfxParseException(string message) : base(message)
+        {
+        }
+
+        public OfxParseException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected OfxParseException(
+            SerializationInfo info,
+            StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
