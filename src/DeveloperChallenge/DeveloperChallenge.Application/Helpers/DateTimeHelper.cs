264eddf7aaf56cd4291074284da16b6f2e1cdd24
Rodrigo <rduclerc@gmail.com>
2020-09-20 11:10:22 -0300

    Refactoring DateTimeHelper

--- src/DeveloperChallenge/DeveloperChallenge.Application/Helpers/DateTimeHelper.cs
+++ src/DeveloperChallenge/DeveloperChallenge.Application/Helpers/DateTimeHelper.cs
@@ -15,15 +15,14 @@ namespace DeveloperChallenge.Application.Helpers
                 if (ofxDateTime.Length < 14)
                     return transactionDateTime;
 
+                var yyyy = Int32.Parse(ofxDateTime.Substring(0, 4));
                 var ss = Int32.Parse(ofxDateTime.Substring(12, 2));
                 var mm = Int32.Parse(ofxDateTime.Substring(10, 2));
                 var hh = Int32.Parse(ofxDateTime.Substring(8, 2));
                 var dd = Int32.Parse(ofxDateTime.Substring(6, 2));
                 var MM = Int32.Parse(ofxDateTime.Substring(4, 2));
-                var yyyy = Int32.Parse(ofxDateTime.Substring(0, 4));
 
-                transactionDateTime = new DateTime(yyyy, MM, dd, hh, mm, ss);
-                return transactionDateTime;
+                return new DateTime(yyyy, MM, dd, hh, mm, ss);
             }
             catch
             {
